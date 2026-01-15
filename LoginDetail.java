5332e0401f06699627be937cd44154bb9e9b7153
rohitojhanwic <rohitojhanwic@gmail.com>
2026-01-15 17:14:50 +0530

    Update LoginDetail.java

--- LoginDetail.java
+++ LoginDetail.java
@@ -1,8 +1,16 @@
 package WimsProject.test;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import dev.failsafe.internal.util.Assert;
+
 
 public class LoginDetail {
 
@@ -22,12 +30,38 @@ public class LoginDetail {
 	        // Fill username and password
 	        driver.findElement(By.xpath("//input[@id='username']")).sendKeys("testing"); //enter username
 	        Thread.sleep(1000);
-	        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Rohit@12345"); //enter password
+	        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("test@1234567890"); //enter password
 	        Thread.sleep(10000);
 
 	        driver.findElement(By.id("j_idt36")).click(); //click on the login button
-	        Thread.sleep(2000); // Wait for the login to complete
+	        //Thread.sleep(2000); // Wait for the login to complete
 	        
+	        // Capture and print the validation message for incorrect captcha
+//	        try {
+//	        	String validationMessage = driver.findElement(By.id("enterCaptcha")).getText(); // Replace with the actual ID or locator of the captcha error message
+//	        	System.out.println("Validation Message: " + validationMessage);
+//	        } catch (Exception e) {
+//	        	System.out.println("Captcha validation message not found or login successful.");
+//	        }
+	        
+	        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+	     // Capture message
+	     WebElement errorMsg = wait.until(
+	         ExpectedConditions.visibilityOfElementLocated(
+	             By.xpath("//*[contains(text(),'Invalid login')]")
+	         )
+	     );
+
+	     // Validate message
+	     
+	     String msg = errorMsg.getText();
+	     System.out.println(msg);
+
+	     //Assert.assertTrue(msg.contains("Invalid login"));
+	     //Assert.assertTrue(msg.contains("valid credentials"));
+	     //Assert.assertTrue(msg.contains("correct captcha"));
+	     
 	        driver.findElement(By.xpath("/html/body/form/div/div/div/div/div/div/ul/li[4]/a")).click(); //select Network Monitoring Management Tab
 	        Thread.sleep(2000);
 	        
@@ -51,5 +85,3 @@ public class LoginDetail {
 	}
 
 }
-
-
